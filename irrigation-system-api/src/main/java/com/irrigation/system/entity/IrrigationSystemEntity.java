40f2f32174f1fd480ed0d1048f6884662b53a091
hariteja-varanasi <varanasi.hariteja@gmail.com>
2022-11-30 00:33:39 +0530

    Committing ER diagram

--- irrigation-system-api/src/main/java/com/irrigation/system/entity/IrrigationSystemEntity.java
+++ irrigation-system-api/src/main/java/com/irrigation/system/entity/IrrigationSystemEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 public class IrrigationSystemEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String description;
